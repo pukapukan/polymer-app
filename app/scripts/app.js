32bdd03d48da91c2f24eb54be8cf7941ed285092
Jason Park <jason.park@laterooms.com>
2014-12-02 18:00:31 +0800

    Done with basic coding

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,17 @@
 (function(document) {
   'use strict';
 
+  window.sharedMixin = {
+    ticksToDateString: function (ticks) {
+      var date = new Date(parseInt(ticks));
+      return date.toLocaleDateString();
+    },
+    ticksToDateTimeString: function (ticks) {
+      var date = new Date(parseInt(ticks));
+      return date.toLocaleString();
+    }
+  };
+
   document.addEventListener('polymer-ready', function() {
     // Perform some behaviour
     console.log('Polymer is ready to rock!');
